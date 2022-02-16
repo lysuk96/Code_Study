20b6953fbfb59983adf93064e52a1b2ec8b7ed9b
이영석 <2016025732@hanyang.ac.kr>
2022-02-16 20:19:30 +0900

    [BOJ] 하노이탑 이동순서 / silver1 / 35분(△) / #42

--- lysuk96/Recursion/09_BOJ_하노이탑이동순서.py
+++ lysuk96/Recursion/09_BOJ_하노이탑이동순서.py
@@ -1,3 +1,5 @@
+# https://www.acmicpc.net/problem/11729
+# 한 경우만 예를들고 고려하자
 def hanoi(start, end, n):
     if n==1:
         print(start, end)
@@ -8,4 +10,5 @@ def hanoi(start, end, n):
     hanoi(6-start-end, end, n-1)
 
 n = int(input())
+print(2**n-1)
 hanoi(1, 3, n)
